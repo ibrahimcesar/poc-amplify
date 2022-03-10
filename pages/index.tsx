e6f19035d2214327ffb3f7e19e97627ac57cd075
Ibrahim Cesar <email@ibrahimcesar.com>
2022-03-10 09:50:07 -0300

    :tada: js

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsconfig from '../src/aws-exports';
+// @ts-nocheck
+import awsconfig from '../aws-exports';
 
 Amplify.configure(awsconfig);
 
