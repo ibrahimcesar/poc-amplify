508d58cb98be3d7ad1ba6b8587c6ca0301003313
Ibrahim Cesar <email@ibrahimcesar.com>
2022-03-10 10:51:20 -0300

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://poc-amplify.vercel.app/",
+        "redirectSignOut": "https://poc-amplify.vercel.app/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
